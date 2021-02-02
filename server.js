27eb2d1044616161e8702202d5be383d05cb918d
homiza <andrewazimoh@gmail.com>
2021-02-02 10:46:02 +0100

    Login form
    login ui requiring userk email and pass

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 //port variable
@@ -6,10 +7,12 @@ const port = process.env.PORT||3000;
 
 app.set('view engine', 'ejs');
 
+//load static assets
+app.use('/static', express.static(path.join(__dirname, 'public')))
 //home route
 
 app.get('/',(req, res)=>{
     res.render('base', {title: "Login System"});
 })
 
-app.listen(port, ()=>{console.log("Listening to the server on http://localhost:3000")});
+app.listen(port, ()=>{console.log("Listening to the server on http://localhost:3000")})
