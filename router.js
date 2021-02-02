f54f98753f214cbbcad1a4b9edf30bb487bebd29
homiza <andrewazimoh@gmail.com>
2021-02-02 13:39:22 +0100

    Logout
    terminates user session and redirects to login page

--- router.js
+++ router.js
@@ -1,19 +1,42 @@
 var express  = require("express"); //to use router meta
 var router = express.Router();
 
-const credetial = {
+const credential = {
     email: "johndoe@gmail.com",
     password: "123"
 }
 //login user route
 router.post('/login', (req, res)=>{
-    if(req.body.email == credetial.email && req.body.password == credetial.password){
+    if(req.body.email == credential.email && req.body.password == credential.password){
         req.session.user = req.body.email;
-        //req.redirect('/dashboard');
-        res.end("Login Successful");
+        res.redirect('/route/dashboard');
     }else{
         res.end("Invalid username")
     }
 });
+
+
+
+//route for dashboard
+router.get('/dashboard', (req, res)=>{
+    if(req.session.user){
+        res.render('dashboard', {user: req.session.user})
+    }else{
+        res.send("Unauthorised user")
+    }
+})
+
+
+//route for logout
+ router.get('/logout', (req, res)=>{
+     req.session.destroy(function(err){
+         if(err){
+            console.log(err);
+            res.send("Error");
+         }else{
+             res.render('base',{title:"Express", logout:"Logout Successful!!"})
+         }
+     })
+ })
 
 module.exports =  router;
